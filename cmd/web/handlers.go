0361055b39e34497f891038faf29d9074c08a01f
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-02-20 19:19:38 -0300

    Project initial structure

---
+++ cmd/web/handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+// Display the gome page
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("hello from Snippetbox"))
+}
+
+// Display a specific snippet
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+// create a new snippet
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("create a new snippet..."))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucasHSantiago/snippetbox
+
+go 1.24.0
